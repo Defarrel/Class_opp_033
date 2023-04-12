230748edcf796f75515dbfd106cbbe963c85adfb
defarrel <Defarrel>
2023-04-12 14:25:21 +0700

    Menyiapkan libary untuk program

--- Class_opp_033/Class_opp_033.cpp
+++ Class_opp_033/Class_opp_033.cpp
@@ -3,23 +3,4 @@ using namespace std;
 
 
 
-class Mahasiswa {
-private:
-	int nim;
-	string nama;
-public:
-	void inputdata() {
-		cout << "Masukan NIM : ";
-		cin >> nim;
-		cout << "Masukan Nama : ";
-		cin >> nama;
-	}
-	void tampildata() {
-		cout << "NIM : " << nim << endl;
-		cout << "Nama : " << nama << endl;
-	}
-};
 
-int main() {
-
-}
