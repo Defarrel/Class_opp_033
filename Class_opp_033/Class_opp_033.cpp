ee58f083428bb5c19118f400813d99466407c1c7
defarrel <Defarrel>
2023-04-12 14:28:26 +0700

    Membuat prosedural inputdata dan tampildata

--- Class_opp_033/Class_opp_033.cpp
+++ Class_opp_033/Class_opp_033.cpp
@@ -7,4 +7,16 @@ class Mahasiswa {
 private:
 	int nim;
 	string nama;
+public:
+	void inputdata() {
+		cout << "Masukan NIM : ";
+		cin >> nim;
+		cout << "Masukan Nama : ";
+		cin >> nama;
+	}
+	void tampildata() {
+		cout << "NIM : " << nim << endl;
+		cout << "Nama : " << nama << endl;
+	}
+};
 
