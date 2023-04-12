f05232176716b22d3cb56a1d5c3c22def4bf741f
defarrel <Defarrel>
2023-04-12 14:23:37 +0700

    Add project files.

---
+++ Class_opp_033/Class_opp_033.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+
+class Mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	void inputdata() {
+		cout << "Masukan NIM : ";
+		cin >> nim;
+		cout << "Masukan Nama : ";
+		cin >> nama;
+	}
+	void tampildata() {
+		cout << "NIM : " << nim << endl;
+		cout << "Nama : " << nama << endl;
+	}
+};
+
+int main() {
+
+}
